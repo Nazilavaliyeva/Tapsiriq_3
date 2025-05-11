623d0e5cad8a85814da60f5491091bb7ba3fc3e4
Nazila Valiyeva (Nazilə Vəliyeva) <Nazilaveliyeva94@gmail.com>
2025-05-11 19:16:56 +0400

    .

--- Tapsiriq_3/Form1.cs
+++ Tapsiriq_3/Form1.cs
@@ -12,10 +12,35 @@ namespace Tapsiriq_3
 {
     public partial class Form1 : Form
     {
+        public readonly ErrorProvider errorProvider = new ErrorProvider();
         public Form1()
         {
             InitializeComponent();
         }
+        //Tam və ya kəsr ədədi  oxuyur ve doğruluğunu yoxlayır
+        private bool TryParseInput(out double result)
+        {
+            string input = textBox1.Text.Trim();
+            //Hərf daxil edilməsinin qarşısını alır 
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                errorProvider.SetError(textBox1, "Zəhmət olmasa ədədi daxil edin");
+                result = 0;
+                return false;
+            }
+            if (double.TryParse(input, out result))
+            {
+                errorProvider.SetError(textBox1, string.Empty);
+                return false;
+            }
+            else
+            {
+                errorProvider.SetError(textBox1, "Zəhmət olmasa düzgün ədəd(tam və ya kəsr) daxil edin.");
+                return false;
+            }
+
+
+        }   
 
         private void musbet_Click(object sender, EventArgs e)
         {
